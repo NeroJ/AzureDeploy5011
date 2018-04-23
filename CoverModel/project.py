0bc94981f2d118341a0cb6c4e8da5954c842eba2
NeroJ <735422760@qq.com>
2018-04-23 15:12:57 +0800

    ..

--- CoverModel/project.py
+++ CoverModel/project.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+import xgboost as xgb
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.cross_validation import KFold
 from sklearn.decomposition import PCA
 from sklearn.externals import joblib
+import copy
 
 class Data_PCAReduction:
     def __init__(self):
@@ -101,7 +106,10 @@ class Training_Model:
             self.Training_SoilArea_To_Coverage(5) # show n-folds validation result
     # validation accuracy: 90%
     def Soil_Type_Training(self, X_Train, Y_Train, X_Test, Y_Test):
-        rf = RandomForestClassifier(n_estimators=self.estimators)
+        #rf = RandomForestClassifier(n_estimators=self.estimators, class_weight='balanced')
+        #rf = GradientBoostingClassifier(n_estimators=self.estimators, learning_rate=0.1, max_depth=10)
+        rf = SVC(class_weight='balanced')
+        #rf = LogisticRegression(penalty='l2', solver='newton-cg', multi_class='multinomial', class_weight='balanced', max_iter=200)
         Model = rf.fit(X_Train, Y_Train)
         Y_Pre = rf.predict(X_Test)
         Model_Accuracy = accuracy_score(Y_Test, Y_Pre)
@@ -109,7 +117,10 @@ class Training_Model:
 
     # Validation Accuracy is 96.5%
     def Training_FinalCover(self, X_Train, Y_Train, X_Test, Y_Test):
-        rf = RandomForestClassifier(n_estimators=self.estimators)
+        #rf = RandomForestClassifier(n_estimators=self.estimators, class_weight='balanced')
+        #rf = GradientBoostingClassifier(n_estimators=self.estimators, learning_rate=0.1, max_depth=10)
+        rf = SVC(class_weight='balanced')
+        #rf = LogisticRegression(penalty='l2', solver='newton-cg', multi_class='multinomial', class_weight='balanced', max_iter=200)
         Model = rf.fit(X_Train, Y_Train)
         Y_Pre = rf.predict(X_Test)
         Model_Accuracy = accuracy_score(Y_Test, Y_Pre)
@@ -142,16 +153,16 @@ class Training_Model:
             print ('Soil Accuracy: ' + str(Soil_Acc))
             print ('Coverage Accuracy: ' + str(Cover_Acc))
             print ('CombinedModel Accuracy: ' + str(Combined_Acc))
-            #break
+            break
         print ('-----------------Final Average Accuracy-----------------')
         print ('Average Accuracy of Soil Model: ' + str(soil_counter/counter))
         print ('Average Accuracy of Coverage Model: ' + str(cover_counter/counter))
         print ('Average Accuracy of Combined Model: ' + str(combined_counter/counter))
 
     def Saving_MoldelPickle(self):
-        X_SoilType = np.array(self.DP.df[self.DP.columns[:-2]])
+        X_SoilType = preprocessing.scale(np.array(self.DP.df[self.DP.columns[:-2]]))
         Y_SoilType = np.array(self.DP.df['Soil_Type'])
-        X_Cover = np.array(self.DP.df[self.DP.columns[:-1]])
+        X_Cover = preprocessing.scale(np.array(self.DP.df[self.DP.columns[:-1]]))
         Y_Cover = np.array(self.DP.df['Cover_Type'])
         rf = RandomForestClassifier(n_estimators=self.estimators)
         Model_SoilType = rf.fit(X_SoilType, Y_SoilType)
@@ -178,17 +189,179 @@ class Training_Model:
         ave = sum(np.array(average))/cv
         print ('average accuracy of the model: '+ str(ave))
 
+
+class Training_ModelXGBoost:
+    def __init__(self, Type = True):
+        self.DP = Data_Preprocessing()
+        #---------Soil&Area_To_Coverage Training Input&Output-------------#
+        self.X_Input_NonePre = np.array(self.DP.df[self.DP.columns[:-2]])
+        self.X_Input = preprocessing.scale(np.array(self.DP.df[self.DP.columns[:-2]]))
+        self.Y_SoilModel_Output = np.array(self.DP.df['Soil_Type'])
+        self.X_Covreage = preprocessing.scale(np.array(self.DP.df[self.DP.columns[:-1]]))
+        self.Y_Coverage = np.array(self.DP.df['Cover_Type'])
+        #----------------Training-------------------#
+        if Type == True:
+            self.Saving_SoilMoldelPickle()
+            self.Saving_CoverMoldelPickle()
+        elif Type == False:
+            self.Training_SoilArea_To_Coverage() # show n-folds validation result
+    # validation accuracy: 90%
+    def Soil_Type_Training(self, X_Train, Y_Train, X_Test, Y_Test):
+        param = {'max_depth': 20,
+                'tree_method':'auto',
+                'eta': 0.3,
+                'silent': 1,
+                'objective': 'multi:softmax',
+                'num_class':40,
+                'eval_metric':'mlogloss'}
+        dtrain = xgb.DMatrix(X_Train, label=Y_Train)
+        dtest = xgb.DMatrix(X_Test, label=Y_Test)
+        evallist = [(dtest, 'eval'), (dtrain, 'train')]
+        num_round = 100
+        Model = xgb.train(param, dtrain, num_round, evallist)
+        Y_Pre = Model.predict(dtest)
+        Model_Accuracy = accuracy_score(Y_Test, Y_Pre)
+        print ('Soil Accuracy: ' + str(Model_Accuracy))
+        return Y_Pre, Model_Accuracy, Model
+
+    # Validation Accuracy is 96.5%
+    def Training_FinalCover(self, X_Train, Y_Train, X_Test, Y_Test):
+        param = {'max_depth': 15,
+                'tree_method':'auto',
+                'eta': 1,
+                'silent': 1,
+                'objective': 'multi:softmax',
+                'num_class':8,
+                'subsample':1,
+                'colsample_bytree':1,
+                'colsample_bylevel':1,
+                'eval_metric':'mlogloss'}
+        dtrain = xgb.DMatrix(X_Train, label=Y_Train)
+        dtest = xgb.DMatrix(X_Test, label=Y_Test)
+        evallist = [(dtest, 'eval'), (dtrain, 'train')]
+        num_round = 100
+        Model = xgb.train(param, dtrain, num_round, evallist)
+        Y_Pre = Model.predict(dtest)
+        Model_Accuracy = accuracy_score(Y_Test, Y_Pre)
+        print ('Coverage Accuracy: ' + str(Model_Accuracy))
+        return Y_Pre, Model_Accuracy, Model
+
+    # Validation Accuracy: 95%
+    def Soil_To_Coverage(self, X_input_tst, Soil_Pre, Y_Coverage_tst, Model):
+        X = preprocessing.scale(np.array([item.tolist()+[j] for item, j in zip(X_input_tst, Soil_Pre)]))
+        Y = Y_Coverage_tst
+        dtest = xgb.DMatrix(X)
+        Y_Pre = Model.predict(dtest)
+        Acc = accuracy_score(Y, Y_Pre)
+        return Y_Pre, Acc
+
+    def Training_SoilArea_To_Coverage(self):
+        kf = KFold(n=len(self.X_Input), n_folds=5, shuffle=True)
+        for tr, tst in kf:
+            Soil_Pre, Soil_Acc, Soil_Model = self.Soil_Type_Training(self.X_Input[tr], self.Y_SoilModel_Output[tr], self.X_Input[tst], self.Y_SoilModel_Output[tst])
+            Cover_Pre, Cover_Acc, Cover_Model = self.Training_FinalCover(self.X_Covreage[tr], self.Y_Coverage[tr], self.X_Covreage[tst], self.Y_Coverage[tst])
+            Combined_Pre, Combined_Acc = self.Soil_To_Coverage(self.X_Input_NonePre[tst], Soil_Pre, self.Y_Coverage[tst], Cover_Model)
+            print ('Soil Accuracy: ' + str(Soil_Acc))
+            print ('Coverage Accuracy: ' + str(Cover_Acc))
+            print ('CombinedModel Accuracy: ' + str(Combined_Acc))
+            break
+
+    def Saving_SoilMoldelPickle(self):
+        param = {'max_depth': 20,
+                'tree_method':'auto',
+                'eta': 0.3,
+                'silent': 1,
+                'objective': 'multi:softmax',
+                'num_class':40,
+                'eval_metric':'mlogloss'}
+        dtrain = xgb.DMatrix(self.X_Input, label=self.Y_SoilModel_Output)
+        dtest = xgb.DMatrix(self.X_Input, label=self.Y_SoilModel_Output)
+        evallist = [(dtest, 'eval'), (dtrain, 'train')]
+        num_round = 10
+        Model = xgb.train(param, dtrain, num_round, evallist)
+        joblib.dump(Model, 'Soil_Model.pkl')
+
+    def Saving_CoverMoldelPickle(self):
+        param = {'max_depth': 15,
+                'tree_method':'auto',
+                'eta': 1,
+                'silent': 1,
+                'objective': 'multi:softmax',
+                'num_class':8,
+                'subsample':1,
+                'colsample_bytree':1,
+                'colsample_bylevel':1,
+                'eval_metric':'mlogloss'}
+        dtrain = xgb.DMatrix(self.X_Covreage, label=self.Y_Coverage)
+        dtest = xgb.DMatrix(self.X_Covreage, label=self.Y_Coverage)
+        evallist = [(dtest, 'eval'), (dtrain, 'train')]
+        num_round = 10
+        Model = xgb.train(param, dtrain, num_round, evallist)
+        joblib.dump(Model, 'Cover_Model.pkl')
+
+
 class Use_Model:
     def __init__(self):
         self.Soil_Model = joblib.load('Soil_Model.pkl')
         self.Cover_Model = joblib.load('Cover_Model.pkl')
 
     def Predict_Soil(self, inputFeatures):
-        inputFeatures = np.array([inputFeatures])
-        Pre_Y = self.Soil_Model.predict(inputFeatures)[0]
+        inputFeatures = preprocessing.scale(np.array([inputFeatures]))
+        dtest = xgb.DMatrix(inputFeatures)
+        Pre_Y = self.Soil_Model.predict(dtest)[0]
         return Pre_Y
 
     def Predict_Cover(self, inputFeatures):
-        inputFeatures = np.array([inputFeatures])
-        Pre_Y = self.Cover_Model.predict(inputFeatures)[0]
+        inputFeatures = preprocessing.scale(np.array([inputFeatures]))
+        dtest = xgb.DMatrix(inputFeatures)
+        Pre_Y = self.Cover_Model.predict(dtest)[0]
+        return Pre_Y
+
+
+class Use_ModelSGBoost:
+    def __init__(self, filename = 'test.csv'):
+        self.filename = filename
+        self.downloadName = filename + '_predicted.csv'
+        self.df = pd.read_csv('uploads/'+self.filename)
+        self.dfOriginal = copy.deepcopy(self.df)
+        self.Soil_Model = joblib.load('Soil_Model.pkl')
+        self.Cover_Model = joblib.load('Cover_Model.pkl')
+        self.df = self.PCA_Reduction(self.df, ['Hillshade_9am','Hillshade_Noon','Hillshade_3pm'], 1, 'Hillshade')
+        self.df = self.PCA_Reduction(self.df, ['Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology'], 1, 'Distance_To_Hydrology')
+        self.X_Input = preprocessing.scale(np.array(self.df[self.df.columns]))
+        if len(self.X_Input[0]) == 9:
+            self.Predict_Cover()
+        if len(self.X_Input[0]) == 8:
+            PreSoil = self.Predict_Soil()
+            self.X_Input = preprocessing.scale(np.array(self.df[self.df.columns]))
+            dtest = xgb.DMatrix(self.X_Input)
+            Pre_Y = self.Cover_Model.predict(dtest)
+            self.dfOriginal.insert(len(list(self.dfOriginal.columns)), 'Soil_Pre', PreSoil)
+            self.dfOriginal.insert(len(list(self.dfOriginal.columns)), 'Cover_Pre', Pre_Y)
+            self.dfOriginal.to_csv('uploads/'+self.downloadName)
+
+    def PCA_Reduction(self, df, columns, components_left, New_name):
+        PCA_Original = np.array(df[columns]).T
+        pca = PCA(n_components = components_left)
+        pca.fit(PCA_Original)
+        for i in range(len(columns)):
+            if i >= components_left:
+                del df[columns[i]]
+            else:
+                PCA_New = pca.components_[i]
+                df[columns[i]] = PCA_New
+                index = list(df.columns).index(columns[i])
+                df.rename(columns = {df.columns[index]: New_name+'_PCA_'+str(i)}, inplace=True)
+        return df
+
+    def Predict_Cover(self):
+        dtest = xgb.DMatrix(self.X_Input)
+        Pre_Y = self.Cover_Model.predict(dtest)
+        self.dfOriginal.insert(len(list(self.dfOriginal.columns)), 'Cover_Pre', Pre_Y)
+        self.dfOriginal.to_csv('uploads/'+self.downloadName)
+
+    def Predict_Soil(self):
+        dtest = xgb.DMatrix(self.X_Input)
+        Pre_Y = self.Soil_Model.predict(dtest)
+        self.df.insert(len(list(self.df.columns)), 'Soil_Pre', Pre_Y)
         return Pre_Y
--- main.py
+++ main.py
@@ -1,14 +1,53 @@
-#from __future__ import print_function
+from __future__ import print_function
 from flask import Flask
 import numpy as np
 import pandas as pd
 from flask import render_template
 from flask import request
 from flask import abort, redirect, url_for
+from flask import send_from_directory
 from CoverModel.project import *
+from werkzeug.utils import secure_filename
 import sys
+import os
 import copy
+
+UPLOAD_FOLDER = os.getcwd()+'/uploads'
+ALLOWED_EXTENSIONS = set(['csv'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads', methods=['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            #print(filename,file=sys.stderr)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            Use_ModelSGBoost(filename = filename)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename +'_predicted.csv'))
+    return render_template('upfile.html')
+
+@app.route('/downloads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 @app.route('/', methods=['POST', 'GET'])
 def GPSData_Prediction():
@@ -35,9 +74,9 @@ def GPSData_Prediction():
             input_Soil = list(np.array(df[columns])[0])
             temp = copy.deepcopy(input_Soil)
             UM = Use_Model()
-            dict_para['SoilType'] = UM.Predict_Soil(input_Soil)
+            dict_para['SoilType'] = int(UM.Predict_Soil(input_Soil))
             input_Cover = temp+[dict_para['SoilType']]
-            dict_para['CoverType'] = UM.Predict_Cover(input_Cover)
+            dict_para['CoverType'] = int(UM.Predict_Cover(input_Cover))
         elif len(dataDict) == 12:
             dict_para['flag']=2
             df = pd.DataFrame(dataDict)
@@ -46,7 +85,7 @@ def GPSData_Prediction():
             columns = list(df.columns)
             input_Cover = list(np.array(df[columns])[0])
             UM = Use_Model()
-            dict_para['CoverType'] = UM.Predict_Cover(input_Cover)
+            dict_para['CoverType'] = int(UM.Predict_Cover(input_Cover))
         else:
             pass
     return render_template('hello.html', dict=dict_para)
